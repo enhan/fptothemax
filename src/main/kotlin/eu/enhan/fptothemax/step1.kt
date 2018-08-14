f45d4c92b83fe36d2107e2535e29b46f5444dbb5
raulraja <raulraja@gmail.com>
2018-08-14 19:38:24 +0200

    interpreters as abstract algebras in terms of MonadDefer

--- src/main/kotlin/eu/enhan/fptothemax/step1.kt
+++ src/main/kotlin/eu/enhan/fptothemax/step1.kt
@@ -3,85 +3,76 @@ package eu.enhan.fptothemax
 import arrow.Kind
 import arrow.core.Option
 import arrow.core.Try
-import arrow.effects.ForIO
 import arrow.effects.IO
-import arrow.effects.console
-import arrow.effects.fRandom
 import arrow.effects.fix
-import arrow.effects.monad
-import arrow.instance
+import arrow.effects.monadDefer
+import arrow.effects.typeclasses.MonadDefer
 import arrow.typeclasses.Monad
 import arrow.typeclasses.binding
-import java.util.Random
-
+import java.util.*
 
 object ORandom : Random()
 
 interface Console<F> {
-    fun putStrLn(s: String): Kind<F, Unit>
-    fun getStrLn(): Kind<F, String>
+  fun putStrLn(s: String): Kind<F, Unit>
+  fun getStrLn(): Kind<F, String>
 }
 
-@instance(IO::class)
-interface IOConsoleInstance : Console<ForIO> {
-
-    override fun putStrLn(s: String): Kind<ForIO, Unit> = IO { println(s) }
-
-    override fun getStrLn(): Kind<ForIO, String> = IO { readLine().orEmpty() }
-
+class ConsoleInstance<F>(val delay: MonadDefer<F>) : Console<F> {
+  override fun putStrLn(s: String): Kind<F, Unit> = delay { println(s) }
+  override fun getStrLn(): Kind<F, String> = delay { readLine().orEmpty() }
 }
 
 interface FRandom<F> {
-    fun nextInt(upper: Int): Kind<F, Int>
+  fun nextInt(upper: Int): Kind<F, Int>
 }
 
-@instance(IO::class)
-interface FRandomInstance: FRandom<ForIO> {
-    override fun nextInt(upper: Int): Kind<ForIO, Int> = IO { ORandom.nextInt(upper) }
+class FRandomInstance<F>(val delay: MonadDefer<F>) : FRandom<F> {
+  override fun nextInt(upper: Int): Kind<F, Int> = delay { ORandom.nextInt(upper) }
 }
 
-class MonadAndConsoleRandom<F>(M: Monad<F>, C: Console<F>, R: FRandom<F>): Monad<F> by M, Console<F> by C, FRandom<F> by R {
-
-}
+class MonadAndConsoleRandom<F>(M: Monad<F>, C: Console<F>, R: FRandom<F>) : Monad<F> by M, Console<F> by C, FRandom<F> by R
 
 object Step1 {
 
-    fun parseInt(s: String): Option<Int> = Try { s.toInt() }.toOption()
+  fun parseInt(s: String): Option<Int> = Try { s.toInt() }.toOption()
 
+  fun <F> MonadAndConsoleRandom<F>.checkContinue(name: String): Kind<F, Boolean> = binding {
+    putStrLn("Do you want to continue, $name?").bind()
+    val input = getStrLn().map { it.toLowerCase() }.bind()
+    when (input) {
+      "y" -> just(true)
+      "n" -> just(false)
+      else -> checkContinue(name)
+    }.bind()
+  }
 
-    fun <F> checkContinue(MC: MonadAndConsoleRandom<F>, name: String): Kind<F,Boolean> = MC.binding {
-        MC.putStrLn("Do you want to continue, $name?").bind()
-        val input = MC.getStrLn().map { it.toLowerCase() }.bind()
-        when (input) {
-            "y" ->  MC.just(true)
-            "n" -> MC.just(false)
-            else -> checkContinue(MC, name)
-        }.bind()
+  fun <F> MonadAndConsoleRandom<F>.gameLoop(name: String): Kind<F, Unit> = binding {
+    val num = nextInt(5).map { it + 1 }.bind()
+    putStrLn("Dear $name, please guess a number from 1 to 5:").bind()
+    val input = getStrLn().bind()
+    parseInt(input).fold({ putStrLn("You did not enter a number") }) { guess ->
+      if (guess == num) putStrLn("You guessed right, $name!")
+      else putStrLn("You guessed wrong, $name! The number was: $num")
+    }.bind()
+    val cont = checkContinue(name).bind()
+    (if (cont) gameLoop(name) else just(Unit)).bind()
+  }
+
+  fun <F> MonadAndConsoleRandom<F>.fMain(): Kind<F, Unit> = binding {
+    putStrLn("What is your name?").bind()
+    val name = getStrLn().bind()
+    putStrLn("Hello $name, welcome to the game").bind()
+    gameLoop(name).bind()
+  }
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val module = IO.monadDefer().run {
+      MonadAndConsoleRandom(this, ConsoleInstance(this), FRandomInstance(this))
     }
-
-    fun <F> gameLoop(MC: MonadAndConsoleRandom<F>, name: String): Kind<F, Unit> = MC.binding {
-        val num = MC.nextInt(5).map { it + 1 }.bind()
-        MC.putStrLn("Dear $name, please guess a number from 1 to 5:").bind()
-        val input = MC.getStrLn().bind()
-        parseInt(input).fold({ MC.putStrLn("You did not enter a number")}){ guess ->
-            if (guess == num) MC.putStrLn("You guessed right, $name!")
-            else MC.putStrLn("You guessed wrong, $name! The number was: $num")
-        }.bind()
-        val cont = checkContinue(MC, name).bind()
-        (if (cont) gameLoop(MC, name) else MC.just(Unit)).bind()
-    }
-
-    fun <F> fMain(MC: MonadAndConsoleRandom<F>): Kind<F, Unit> = MC.binding {
-        MC.putStrLn("What is your name?").bind()
-        val name = MC.getStrLn().bind()
-        MC.putStrLn("Hello $name, welcome to the game").bind()
-        gameLoop(MC, name).bind()
-    }
-
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val r = fMain(MonadAndConsoleRandom(IO.monad(), IO.console(), IO.fRandom()))
-        r.fix().unsafeRunSync()
-    }
+    val r = module.fMain()
+    r.fix().unsafeRunSync()
+  }
 
 }
